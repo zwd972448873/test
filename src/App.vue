54b4d319a7d0077bf5074ed1dbc30f56140c34c8
吴宇婷 <wuyt@wingoht.com>
2022-09-01 21:02:30 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="container">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside>Aside</el-aside>
+        <el-main>
+          <el-scrollbar class="show-box">
+            <router-view></router-view>
+          </el-scrollbar>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss" scoped>
+.container {
+  .el-header {
+    height: 45px !important;
+    background-image: linear-gradient(to right, #5fc4ff, #428af5);
+  }
+  .el-aside {
+    width: 200px !important;
+    background-color: #042046;
+  }
+  .el-main {
+    padding: 0;
+  }
+  :deep(.el-scrollbar__wrap) {
+    overflow-x: hidden !important;
+  }
+  .show-box {
+    height: calc(100vh - 45px);
+  }
 }
 </style>
---
+++ src/views/list/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list">
+    <div v-for="item in 200" :key="item">1234554532</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name:'list',
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.list{
+  background: greenyellow;
+}
+</style>
